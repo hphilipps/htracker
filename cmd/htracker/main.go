1662dada8283359cb82f00fcde79912cb81ed4fe
Henri Philipps <henri.philipps@gmail.com>
2022-12-18 22:15:10 +0100

    fix typo in main

--- cmd/htracker/main.go
+++ cmd/htracker/main.go
@@ -92,7 +92,7 @@ func main() {
 	subscriptionSvc.Subscribe("email1", &htracker.Site{URL: "http://httpbin.org/anything/2"})
 	subscriptionSvc.Subscribe("email2", &htracker.Site{URL: "http://httpbin.org/anything/2"})
 
-	dbgLogger := slog.New(slog.HandlerOptions{Level: slog.DebugLevel}.NewTextHandler(os.Stdout))
+	dbgLogger := slog.New(slog.HandlerOptions{Level: slog.LevelDebug}.NewTextHandler(os.Stdout))
 
 	w := watcher.NewWatcher(archive, subscriptionSvc, watcher.WithInterval(5*time.Second), watcher.WithLogger(dbgLogger))
 
