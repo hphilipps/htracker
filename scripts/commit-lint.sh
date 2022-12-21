1ecc7342ad4ced6d3d47693343014c2cc2e89462
Henri Philipps <henri.philipps@gmail.com>
2022-12-21 19:26:45 +0100

    ci: replace npm commit-lint with simple shell script

---
+++ scripts/commit-lint.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+regexp='^(?:ci|feat|fix|docs|style|refactor|revert|perf|test|chore)\(?(?:\w+|\s|\-|_)?\)?:\s\w+'
+
+while read line; do
+	# ignore merge requests
+	if echo "$line" | grep -qE "^Merge branch \'"; then
+		continue
+	fi
+
+	# check semantic versioning scheme
+	if ! echo "$line" | grep -qE $regexp; then
+		echo
+		echo "Your commit title did not follow semantic versioning: '$line'"
+		echo
+		echo "regexp: $regexp"
+		echo "Please see https://github.com/angular/angular.js/blob/master/DEVELOPERS.md#commit-message-format"
+		exit 1
+	fi
+
+	echo "semantic commit title ok: $line"
+done
