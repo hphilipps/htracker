2878050d95fc46a52acd9ad8ce1cbd8cc389da4e
Henri Philipps <henri.philipps@gmail.com>
2023-01-06 18:22:44 +0100

    style: add some code comments

--- http/json.go
+++ http/json.go
@@ -11,10 +11,7 @@ import (
 	"gitlab.com/henri.philipps/htracker/endpoint"
 )
 
-type StatusCoder interface {
-	StatusCode() int
-}
-
+// createJSONHandler is a generic HandlerFunc factory.
 func createJSONHandler[Req endpoint.Requester, Resp endpoint.Responder](ep endpoint.Endpoint[Req, Resp]) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		ctx := req.Context()
@@ -44,12 +41,18 @@ func createJSONHandler[Req endpoint.Requester, Resp endpoint.Responder](ep endpo
 	}
 }
 
+// decodeHTTPJSONRequest is a generic decoder for JSON HTTP requests.
+// TODO: Implement input validation in each Requester implementation,
+// right now we only do a basic nil check there.
 func decodeHTTPJSONRequest[Req endpoint.Requester](_ context.Context, r *http.Request) (Req, error) {
 	var req Req
 	err := json.NewDecoder(r.Body).Decode(&req)
 	return req, err
 }
 
+// encodeHTTPJSONResponse is a generic response encoder. It is using Failed() to
+// determine how to encode domain-specific errors and the StatusCode() to create
+// the right http response code.
 func encodeHTTPJSONResponse[Resp endpoint.Responder](ctx context.Context, w http.ResponseWriter, response Resp) error {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	if err := response.Failed(); err != nil {
